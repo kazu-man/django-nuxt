b1452dc4a65dffb8b1c74e4304c89474c7c48373
ユーザ <0hy53152387219x@mail.com>
2021-10-31 22:16:37 +0900

    dokcer-composeを追加

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -107,4 +107,14 @@ export default {
     extend (config, ctx) {
     }
   },
+  server: {
+    port: 3000,
+    host: '0.0.0.0',  
+  },
+  watchers: {
+    webpack: {
+      poll: true
+    }
+  }
+
 }
